c312f4f9f57ed94c8758cac4d3776ef3995b9dc4
Simone Cesano <simonecesano@libero.it>
2021-05-05 11:22:13 +0200

    Paging works ok - still issue with partially visible elements being cut off

--- public/components/paged.vue
+++ public/components/paged.vue
@@ -2,15 +2,33 @@
 * { font-family: Courier }
 tr                { background-color: #aaaaaa }
 tr:nth-child(odd) { background-color: #dddddd; }
+
+/* tr, tr * { opacity: 0 } */
 td { padding: 12px }
+
+table { table-layout: fixed; }
+td:nth-child(0) { column-width: 4em;  }
+td:nth-child(2) { column-width: 4em;  }
+td:nth-child(3) { column-width: 4em;  }
+td:nth-child(4) { column-width: 12em;  }
+td:nth-child(4) { column-width: 24em;  }
+td:nth-child(5) { column-width: 8em;  }
 </style>
 <template>
   <div>
+    <div>min row: {{ minRow }}</div>
+    <div>max row: {{ maxRow }}</div>
+    <div @click="nextPage">next</div>
+    <div @click="prevPage">prev</div>
     <table>
+      <col style="width:4em">
+      <col style="width:4em">
+      <col style="width:12em">
+      <col style="width:64em">
+      <col style="width:8em">
       <tbody :key="update">
-	<tr :class="(i + 1) == lastItem ? 'last' : undefined" v-for="(r, i) in items.slice(0, lastItem)">
-	  <td>{{ i }}</td>
-	  <td>{{ r.entity_id }}</td>
+	<tr :data-idx="r.idx" :class="(i + 1) == lastItem ? 'last' : undefined" v-for="(r, i) in items.slice(minRow, maxRow + 1)">
+	  <td>{{ r.idx }}</td>
 	  <td>{{ r.entity_class }}</td>
 	  <td>{{ r.map_category }}</td>
 	  <td>{{ (r.entity_data || {}).label }}</td>
@@ -24,37 +42,73 @@ td { padding: 12px }
 module.exports = {
     data: function () {
 	return {
+	    pages: [],
 	    items:   [ ],
 	    lastItem: 40,
 	    update: 0,
 	    observer: undefined,
+	    minRow: 0,
+	    maxRow: 40,
+	    currPage: 0,
 	};
     },
     mounted: function(){
 	var c = this;
-	axios.get('https://www.wikidata.org/wiki/Special:EntityData/Q28937423.json')
-	    .then(d => console.log(d.data))
-	    .catch(e => console.log(e))
-
+	console.log(c);
 	axios.get('/items')
 	    .then(d => {
 		console.log(JSON.stringify(d.data.value[0]));
-		c.items = d.data.value;
+		c.items = d.data.value.map((p, i) => { p.idx = i; return p });
+		console.log(JSON.stringify(c.items[0]));
 
-		Vue.nextTick(function () {
-		    c.startLazyLoader();
-		})
 		c.loadEntities()
 	    })
 	    .catch(e => console.log(e))
+	setInterval(function(){
+	    c.getScrollTops()
+	}, 300);
     },
     destroyed: function(){
 
     },
     methods: {
+	nextPage: function(){
+	    var c = this;
+	    this.minRow = this.maxRow + 1
+	    this.maxRow = this.maxRow + 40;
+	    this.loadEntities()
+	    this.currPage = this.currPage + 1;
+	},
+	prevPage: function(){
+	    if (this.currPage > 0) {
+		this.minRow = this.pages[this.currPage - 1][0];
+		this.maxRow = this.pages[this.currPage - 1][1];
+		this.currPage = this.currPage - 1
+	    }
+	},
+	isInViewport: function(element) {
+	    const rect = element.getBoundingClientRect();
+	    return (
+		rect.top >= 0 &&
+		    rect.left >= 0 &&
+		    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	    );
+	},
+	getScrollTops: function(){
+	    var c = this;
+	    var rows = Array.from(document.querySelectorAll('tr'))
+		.map((row, i) => [ i, c.isInViewport(row), row.dataset.idx ])
+	    c.minRow = Math.min(...rows.filter(r => r[1]).map(r => r[2]));
+	    c.maxRow = Math.max(...rows.filter(r => r[1]).map(r => r[2]));
+	    c.pages[c.currPage] = [c.minRow, c.maxRow];
+	},
+	scrollTop: function(e){
+	    console.log('scrollTop', e);
+	},
 	loadEntities: function(){
 	    var c = this;
-	    c.items.slice(0, c.lastItem)
+	    c.items.slice(0, c.maxRow + 40)
 		.forEach((i, k) => {
 		    if (!i.entity_data) {
 			// axios.get('https://www.wikidata.org/wiki/Special:EntityData/' + i.entity_id + '.json')
@@ -63,7 +117,6 @@ module.exports = {
 				i.entity_data = d.data;
 				if (!(k % 10)) {
 				    c.update = Math.random()
-				    Vue.nextTick(function () { c.observer.observe(document.querySelector('.last')) })
 				}
 			    })
 			    .catch(e => console.log(e))
@@ -72,22 +125,8 @@ module.exports = {
 	    c.update = c.update;
 	},
 	observerCallback: function(entries, observer){
-	    var c = this;
-            if (entries.filter(entry => entry.isIntersecting).length) {
-		c.lastItem = c.lastItem + 20;
-		console.log('loading ' + c.lastItem)
-		c.loadEntities()
-		Vue.nextTick(function () {
-			observer.observe(document.querySelector('.last'))
-		})
-            }
 	},
 	startLazyLoader: function(){
-	    var c = this;
-            let options = { rootMargin: '160px', threshold: 0 }
-            var observer = new IntersectionObserver(c.observerCallback, options);
-            observer.observe(document.querySelector('.last'))
-	    c.observer = observer;
 	},
     },
 }
