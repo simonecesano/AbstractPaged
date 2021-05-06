b457eda64fd657bade23c406225b98cff81a618d
Simone Cesano <simonecesano@libero.it>
2021-05-06 07:37:09 +0200

    First commit

---
+++ public/components/many.vue
@@ -0,0 +1,27 @@
+<style scoped>
+</style>
+<template>
+  <div>
+    <table>
+      <item v-for="r in items" _key="r" :entity_id="r"></item>
+    </table>
+  </div>
+</template>
+<script>
+module.exports = {
+    data: function () {
+	return {
+	    items: ['Q603939', 'Q1115154', 'Q3674237', 'Q22980015', 'Q2479053']
+	};
+    },
+    mounted: function(){
+    },
+    destroyed: function(){
+    },
+    methods: {
+    },
+    components: {
+        item: httpVueLoader('components/item.vue'),
+    },
+}
+</script>
